302e8012d6a2933b98f061b6e82d8ab16c86a1cb
Asnow <ganrn@vip.qq.com>
2023-05-21 21:08:37 +0800

    feat: node20支持

--- hook.mjs
+++ hook.mjs
@@ -1,8 +1,11 @@
 export * from "./out/node-loader/ts.resolver.mjs";
 export * from "./out/node-loader/ts_transpile.load.mjs" //编译ts
-import "./require.cjs"
 
-
-// export function globalPreload(context) {
-//     return `import "./require.cjs"`;
-// } 
+//node v20 兼容
+export function globalPreload(context) {
+    return `
+    const { createRequire } = getBuiltin('module');
+    const { cwd } = getBuiltin('process');
+    const require = createRequire("${import.meta.url}")
+    require("./require.cjs")`;
+} 
