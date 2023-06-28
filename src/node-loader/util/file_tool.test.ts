1ec3c255d7eca9e9d1acda33489241c793abc8d6
ganrinong <ganrinong@datagrand.com>
2023-06-28 11:24:50 +0800

    fix: 路径向上搜索的 bug

--- src/node-loader/util/file_tool.test.ts
+++ src/node-loader/util/file_tool.test.ts
@@ -1,23 +1,40 @@
 import { upSearch } from "./file_tool.js";
 import { it, describe, expect } from "vitest";
+import { win32, posix } from "node:path";
+function windowPathToUnixPath(path: string) {
+    if (path.length <= 2 && /^[a-zA-XZ]:[\\/]?$/.test(path)) return posix.resolve("/" + path);
+    if (win32.isAbsolute(path)) return posix.resolve("/" + path);
+    return posix.resolve(path);
+}
+const windows = {
+    "A:/dg/sd/dd": ["A:\\dg\\sd\\dd", "A:\\dg\\sd", "A:\\dg", "A:"],
+    "A:/a.js": ["A:\\a.js", "A:"],
+    "/": ["\\"],
+    "/A:": ["\\A:", "\\"],
+};
+const unix = {
+    "/dg/sd/dd": ["/dg/sd/dd", "/dg/sd", "/dg", "/"],
+    "/a.js": ["/a.js", "/"],
+    "/": ["/"],
+};
+const eachArg = [
+    { name: "windows", cases: windows },
+    { name: "unix", cases: unix },
+];
+describe.each(eachArg)("upSearch", function ({ cases, name }) {
+    const os = name === "unix" ? posix : win32;
+    const fullCase: [string, string[]][] = [];
+    it.each(Object.entries(cases))(name + "返回第一项", function (inParameter, expects) {
+        let res: string[] = Array.from(upSearch(inParameter, undefined, os));
 
-describe("upSearch", function () {
-    it("不使用用第一项", function () {
-        let res: string[] = Array.from(upSearch("A:/dg/sd/dd", false));
-        expect(res).toEqual(["A:\\dg\\sd", "A:\\dg", "A:"]);
+        expect(res, '输入："' + inParameter + '"').toEqual(expects);
+
+        expects.shift();
+        fullCase.push([inParameter, res]);
     });
-    it("使用用第一项", function () {
-        let res: string[] = Array.from(upSearch("A:/dg/sd/dd"));
-        expect(res).toEqual(["A:\\dg\\sd\\dd", "A:\\dg\\sd", "A:\\dg", "A:"]);
-    });
-    describe("根目录", function () {
-        it("不使用用第一项", function () {
-            let res: string[] = Array.from(upSearch("A:/", false));
-            expect(res).toEqual([]);
-        });
-        it("使用用第一项", function () {
-            let res: string[] = Array.from(upSearch("A:/"));
-            expect(res).toEqual(["A:\\"]);
-        });
+    //不返回第一项实际就是“返回第一项”的结果去除第一个
+    it.each(fullCase)(name + "不返回第一项", function (inParameter, expects) {
+        let res: string[] = Array.from(upSearch(inParameter, false, os));
+        expect(res).toEqual(expects);
     });
 });
--- src/node-loader/util/file_tool.ts
+++ src/node-loader/util/file_tool.ts
@@ -1,15 +1,17 @@
 import * as Path from "node:path";
-export function* upSearch(checkPath: string, first = true) {
-    let { root, dir, base } = Path.parse(Path.resolve(checkPath));
-    const sep = Path.sep;
-    checkPath = base === "" ? dir : dir + sep + base;
+export function* upSearch(checkPath: string, first = true, nodePath = Path) {
+    let { root, dir, base } = nodePath.parse(nodePath.resolve(checkPath));
+    const sep = nodePath.sep;
+    if (base === "") checkPath = dir;
+    else checkPath = dir === root ? root + base : dir + sep + base;
     if (first) yield checkPath;
 
     let separatorIndex: number;
     while (checkPath.length > root.length) {
         separatorIndex = checkPath.lastIndexOf(sep);
         checkPath = checkPath.slice(0, separatorIndex);
-        yield checkPath;
+        if (separatorIndex > 0) yield checkPath;
+        else yield root;
     }
     return;
 }
